fad87fa228a3811b1d2988166e1b67d5457d04af
Bodunrin David <bodunrindavidbond@gmail.com>
2022-03-18 14:17:44 +0100

    fixed the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,47 @@
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import styles from './footer.module.css'
 function Footer() {
   return (
     <footer>
-      <div className={styles.icons}>
-        <FaGithub size={24} fill="#3455DB" style={{ cursor: 'pointer' }} />
-        <FaLinkedin size={24} fill="#3455DB" style={{ cursor: 'pointer' }} />
-        <FaTwitter size={24} fill="#3455DB" style={{ cursor: 'pointer' }} />
+      <div className={styles.foot}>
+        <div id={styles.footer}>
+          <div id={styles.footer_name}>
+            <h2>David Bodunrin</h2>
+            <p>&copy; 2022 Built and designed by David Bodunrin</p>
+          </div>
+
+          <div id={styles.footer_links}>
+            <h3>Links</h3>
+            <ul id={styles.links}>
+              <li>
+                <Link to="/about"> About </Link>
+              </li>
+              <li>
+                <Link to="/blog"> Blog </Link>
+              </li>
+              <li>
+                <Link to="/projecs"> Projects </Link>
+              </li>
+              <li>
+                <Link to="/contact"> Contact </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={styles.icons}>
+          <h4>Get in touch </h4>
+          <div id={styles.social}>
+            <FaGithub size={24} fill="#fafaff" style={{ cursor: 'pointer' }} />
+            <FaLinkedin
+              size={24}
+              fill="#fafaff"
+              style={{ cursor: 'pointer' }}
+            />
+            <FaTwitter size={24} fill="#fafaff" style={{ cursor: 'pointer' }} />
+          </div>
+        </div>
       </div>
       <p className={styles.text}>&copy; David Bodunrin - All Rights reserved</p>
     </footer>
